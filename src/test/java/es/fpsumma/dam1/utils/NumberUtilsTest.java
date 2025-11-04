a9c23cb07f8c44ad7685cd3de6640ec44bd1ac65
lunhect <hlunamoreno17@gmail.com>
2025-11-04 12:29:39 +0100

    Update NumberUtilsTest.java

--- src/test/java/es/fpsumma/dam1/utils/NumberUtilsTest.java
+++ src/test/java/es/fpsumma/dam1/utils/NumberUtilsTest.java
@@ -1,10 +1,32 @@
 package es.fpsumma.dam1.utils;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class NumberUtilsTest {
+@Test
+public void divisionOK() {
+
+int resultado = NumberUtils.divison(2, 2);
+assertEquals(1, resultado);
+
+
+
+}
+
+
+@Test
+public void segundadivOK() {
+
+int resultado = NumberUtils.divison(5, 8);
+assertEquals(1, resultado);
+
+
+
+}
+
 
    
 }
